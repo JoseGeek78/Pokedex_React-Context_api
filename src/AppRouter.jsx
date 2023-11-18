bdb1e3b3628d942baa41c7720512ef766f790111
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-18 08:57:05 +0100

    Update

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
+import { HomePage } from "./pages/HomePage";
+import { PokemonPage } from "./pages/PokemonPage";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={Navigation}>
-        <Route index element={HomePage} />
-        <Route path="pokemon/:id" element={PokemonPage} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<HomePage />} />
+        <Route path="pokemon/:id" element={<PokemonPage />} />
       </Route>
     </Routes>
   );

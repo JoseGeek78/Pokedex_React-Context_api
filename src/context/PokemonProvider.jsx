497dd502174e69bcac94f0b29d1be638cd3b8ea7
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-20 18:07:56 +0100

    Update

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,14 +1,20 @@
-import { PokemonContext } from "./PokemonContext"
+import { PokemonContext } from "./PokemonContext";
 
 export const PokemonProvider = ({ children }) => {
 
+  // Llamar 50 pokemones de la API
 
+  const getAllPokemons = async (limit = 50) => {
+		
+	};
 
   return (
-    <PokemonContext.Provider value={{
-			numero: 0
-		}}>
-			{children}
-		</PokemonContext.Provider>
-  )
-}
+    <PokemonContext.Provider
+      value={{
+        numero: 0,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+};

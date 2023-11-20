5c1b241c3ffa58496da5e2185de8507db50d1d38
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-20 17:54:38 +0100

    Update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ export const Navigation = () => {
 			<header className='container'>
 				<Link to='/' className='logo'>
 					<img
-						src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png'
+						src='src/assets/Pokédex_logo.png'
 						alt='Logo Pokedex'
 					/>
 				</Link>

6a671e5110092240236d5bcd023b8b2093eb530e
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-23 05:31:26 +0100

    Update

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react";
 import { PokemonContext } from "./PokemonContext";
 
 export const PokemonProvider = ({ children }) => {
 
+  const [allPokemons, setAllPokemons] = useState({})
+  const [offset, setOffset] = useState(0);
+
   // Llamar 50 pokemones de la API
 
   const getAllPokemons = async (limit = 50) => {
-		
-	};
+    const baseURL = "https://pokeapi.co/api/v2/";
+
+    const res = await fetch(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await res.json();
+
+    const promises = data.result.map(async(pokemon) => {
+      const res = await fetch(pokemon.url)
+      const data = await res.json()
+      return data
+    })
+
+    const results = await Promise.all(promises)  
+    
+    setAllPokemon(results)
+  };
+
+  useEffect(() => {
+    getAllPokemons()
+   }, [])
+  
 
   return (
     <PokemonContext.Provider

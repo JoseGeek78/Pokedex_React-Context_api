295e6c27c64a475f766c66fcb2cc78a64f834ba4
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-27 20:27:21 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+
+import react from '@vitejs/plugin-react-swc';
+
+// Resto de la configuración...
+
 
 // https://vitejs.dev/config/
 export default defineConfig({

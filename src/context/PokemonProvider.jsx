db594348a9239227329c6782cb463fd57e926c01
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-27 05:37:15 +0100

    Update

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -4,9 +4,10 @@ import { PokemonContext } from "./PokemonContext";
 export const PokemonProvider = ({ children }) => {
 
   const [allPokemons, setAllPokemons] = useState({})
+  const [globalPokemons, setGlobalPokemons] = useState([])
   const [offset, setOffset] = useState(0);
 
-  // Llamar 50 pokemones de la API
+  // Llamar 50 pokemon de la API
 
   const getAllPokemons = async (limit = 50) => {
     const baseURL = "https://pokeapi.co/api/v2/";
@@ -24,8 +25,30 @@ export const PokemonProvider = ({ children }) => {
 
     const results = await Promise.all(promises)  
     
-    setAllPokemon(results)
+    setAllPokemons(results)
   };
+
+  //Llamar a todos los Pokemon
+
+  const getGlobalPokemons = async() => {
+    const baseURL = "https://pokeapi.co/api/v2/"
+
+    const res = await fetch(
+      `${baseURL}pokemon?limit=100000&offset=0`
+    );
+    const data = await res.json();
+
+    const promises = data.result.map(async(pokemon) => {
+      const res = await fetch(pokemon.url)
+      const data = await res.json()
+      return data
+    })
+
+    const results = await Promise.all(promises)  
+    
+    setGlobalPokemons(results)
+  };
+  
 
   useEffect(() => {
     getAllPokemons()
